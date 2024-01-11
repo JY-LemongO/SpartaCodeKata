f193b5be20fbc5a6601826efd85c32744315926b
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-11 09:42:38 +0900

    목 구하기
    풀이

--- CodeKata/CodeKata/Program.cs
+++ CodeKata/CodeKata/Program.cs
@@ -3,15 +3,21 @@
     internal class Program
     {
         #region 두 수의 차
-        //static int solution(int num1, int num2)
-        //{            
-        //    return num1 - num2;
-        //}
+        static int solution1(int num1, int num2)
+        {
+            return num1 - num2;
+        }
         #endregion
         #region 두 수의 곱
-        int solution(int num1, int num2)
-        {            
+        static int solution2(int num1, int num2)
+        {
             return num1 * num2;
+        }
+        #endregion
+        #region 몫 구하기
+        static int solution3(int num1, int num2)
+        {            
+            return num1 / num2;
         }
         #endregion
         static void Main(string[] args)

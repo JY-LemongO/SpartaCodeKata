6f320302cc46dedd45729a339e9e8837130647ff
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-11 09:23:50 +0900

    2. 두 수의 곱
    풀이

--- CodeKata/CodeKata/Program.cs
+++ CodeKata/CodeKata/Program.cs
@@ -3,13 +3,18 @@
     internal class Program
     {
         #region 두 수의 차
-        static int solution(int num1, int num2)
+        //static int solution(int num1, int num2)
+        //{            
+        //    return num1 - num2;
+        //}
+        #endregion
+        #region 두 수의 곱
+        int solution(int num1, int num2)
         {            
-            return num1 - num2;
-        }    
-    #endregion
-
-    static void Main(string[] args)
+            return num1 * num2;
+        }
+        #endregion
+        static void Main(string[] args)
         {
             
         }

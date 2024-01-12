8f3ae17e694209790b2f70dd300199f24b90e435
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-12 09:24:01 +0900

    Re: 8. 각도기

--- CodeKata/CodeKata/Program.cs
+++ CodeKata/CodeKata/Program.cs
@@ -47,7 +47,7 @@
         #region 8. 각도기
         static int solution(int angle)
         {
-            
+            return angle > 0 ? angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4 : -1;
         }
         #endregion
 

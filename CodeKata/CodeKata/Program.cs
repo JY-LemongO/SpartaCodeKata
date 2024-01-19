be75e1baed6185d36fcc9fcd5c093225576a0c4c
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-19 09:12:55 +0900

    이제 안쓰지만 일단추가

--- CodeKata/CodeKata/Program.cs
+++ CodeKata/CodeKata/Program.cs
@@ -70,6 +70,21 @@
             return answer / numbers.Length;
         }
         #endregion
+        
+        static int solution(int num)
+        {
+            double x = num;
+            int count = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                if (x == 1)
+                    return count;
+                x = x % 2 == 0 ? x / 2 : x * 3 + 1;
+                count++;
+            }
+            return -1;
+        }
+
         static void Main(string[] args)
         {
             
